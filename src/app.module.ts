49402e50f1ebac926c02d135875ccd2faeb52f3c
wwweon <wwweon@prodlestudio.com>
2024-08-22 19:21:27 +0900

    20240822 Add Client login/signup by 3 ways. (google, kakao, email) Add Broker/Provider login/signup.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+
+import { ClientModule } from './client/client.module';
+import { Client } from './client/entities/client.entity';
+
+import { BrokerModule } from './broker/broker.module';
+import { ContractModule } from './contract/contract.module';
+import { ProviderModule } from './provider/provider.module';
+import { ChatModule } from './chat/chat.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // for dev. if prd, false
+    }),
+    AuthModule,
+    ClientModule,
+    BrokerModule,
+    ContractModule,
+    ProviderModule,
+    ChatModule,
+    PaymentModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+/*
+  app.enableCors({
+    origin: 'http://192.168.0.131.nip.io:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+*/
+
+  app.enableCors()
   await app.listen(3000);
 }
 bootstrap();
