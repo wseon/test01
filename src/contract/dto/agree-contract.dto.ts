78038ccc6ad21e4d840fa428c34db7c7dd4af1fa
frontmulti <frontmulti@gmail.com>
2024-09-09 23:16:12 +0900

    :bulb: Contract > Dto Swagger 명세 추가

--- src/contract/dto/agree-contract.dto.ts
+++ src/contract/dto/agree-contract.dto.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber } from "class-validator";
+
 export class AgreeContractDto {
+
+  @ApiProperty({ description: 'Contract ID'})
+  @IsNotEmpty()
+  @IsNumber()
   contractId: number;
 }
--- src/contract/dto/request-contract.dto.ts
+++ src/contract/dto/request-contract.dto.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber } from "class-validator";
+
 export class RequestContractDto {
+  
+  @ApiProperty({ description: 'Request ID'})
+  @IsNotEmpty()
+  @IsNumber()
   requestId: number;
 }
